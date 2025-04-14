83173c3192e96ed7a3193a0a76a2f978485d6c00
Kurler3 <miguelbento2000@gmail.com>
2025-04-14 09:26:51 +0200

    Cleaning

--- parser/url_parser.go
+++
@@ -1,9 +0,0 @@
-package parser
-
-// TODO - Parser of the method flag.
-func ParseUrl(method string) (string, error) {
-
-	//TODO - Check if is inside the list of available methods.
-
-	return method, nil
-}
